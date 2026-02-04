aa1e7d408c545538883246ae816b9ae158574cc1
Pandi Obsidian <>
2026-02-04 20:42:12 +0100

    Wired up LocationTask Handler to form-validation logic

--- knk-core/build.gradle.kts
+++ knk-core/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
+    implementation("com.google.code.gson:gson:2.10.1")
+    
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
